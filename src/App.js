316e4ad6d9ddd8c360a8200e040eb460b653dde9
Vincent Pinas <ikbengerrit92@gmail.com>
2022-01-13 19:53:48 +0100

    :bug: fix added components back in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
 
+// Import components
+import Pagination from './Components/Pagination/Pagination';
+
+const pages = [
+  {name: "Ásbjörn Melisa",personality: "Bubbly", slug: "asbjorn-melisa"}, 
+  {name: "Lizbeth Anfisa",personality: "Analytic", slug: "lizbeth-anfisa"},
+  {name: "Ilija Justino",personality: "Cool headed", slug: "ilija-justino"}, 
+  {name: "Lotte Rafael",personality: "Energetic", slug: "lotte-rafael"},
+  {name: "Gunder Zsiga",personality: "Aggressive", slug: "gunder-zsiga"},
+]
+
 function App() {
   return (
     <>
-      -
+      <Pagination pages={pages} />
     </>
   );
 }
